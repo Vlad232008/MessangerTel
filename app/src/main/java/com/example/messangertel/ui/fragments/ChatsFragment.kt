19419fb034329dbc890180a51f599db2a64ae288
Rogachev_vv <vlad2320082009@gmail.com>
2022-11-30 17:55:38 +0400

    123

---
+++ app/src/main/java/com/example/messangertel/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.messangertel.ui.fragments
+
+import com.example.messangertel.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/example/messangertel/ui/fragments/SettingFragment.kt
@@ -0,0 +1,11 @@
+package com.example.messangertel.ui.fragments
+
+import com.example.messangertel.R
+
+class SettingFragment : BaseFragment(R.layout.fragment_setting) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
